cd774f56fce5cb131ff01c9ae6aedd79f9c90df3
Karthik T <karthikt.holmes+github@gmail.com>
2013-11-16 17:32:08 +0800

    First!

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'coffee-script', '>= 2.2.0'
+gem 'jsmin', '>= 1.0.1'
+gem 'sass', '>= 3.2.9'
+gem 'sinatra', '>= 1.4.3'
+gem 'sinatra-chassis', '>= 1.1.1', require: 'sinatra/chassis'
+gem 'sinatra-contrib', '>= 1.4.0', require: 'sinatra/contrib'
+
+# gem 'chassis-datamapper'
+# gem 'chassis-pony'
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'bundler/setup'
+Bundler.require
+
+require 'sinatra/chassis/helpers'
+
+require 'sinatra/reloader' if development?
+
+set :session_secret, 'secret123'
+
+require_directory([
+  'config',
+  'settings',
+  'modules',
+  'helpers',
+  'libraries',
+  'models',
+  'controllers',
+  'routes'
+])
+
+get '/?' do
+  erb "Hello world!"
+end
