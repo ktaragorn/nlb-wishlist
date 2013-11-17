88adc6dbac10679db5fa284ec55e9f107c5b99ba
Karthik T <karthikt.holmes+github@gmail.com>
2013-11-17 11:38:58 +0800

    Ignore /tmp/pages/* NlbPage class methods done, they take url of book. Since I cant figure out the url logic, I use full url and hash of it for file name

--- models/nlb_page.rb
+++ models/nlb_page.rb
@@ -2,20 +2,33 @@
 # Caches the page on disk to prevent multiple network accesses
 class NlbPage
   PAGE_STORE_PATH = "tmp/pages/"
-  def self.get(book)
-    # check cache
+  class <<self
+    def get(book_url)
+      # check cache
+      unless File.exist?(file_name book_url)
+        # download page
+        system ("curl #{book_url} > #{file_name book_url}")
+      end
 
-    # download page
+      # create object
+      File.open(file_name book_url) {|f| new f}
+    end
 
-    # create object
-  end
+    def clear
+      # clears cached pages
+      File.delete *Dir.glob("#{PAGE_STORE_PATH}/*")
+    end
 
-  def self.clear
-    # clears cached pages
+    private
+      def file_name(book_url)
+        "#{PAGE_STORE_PATH}/#{book_url.hash}"
+      end    
   end
 
   def initialize(file)
     # parse details
+    @a = 1
+    @b = 2
   end
 
 
